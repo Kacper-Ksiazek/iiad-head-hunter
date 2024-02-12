5c9094f86fc884e92130e313213e6605e436655c
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-02-12 12:34:20 +0100

    repo init 🌈

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { MetaTags } from 'svelte-meta-tags'
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Navigation from "@/lib/Navigation";
+</script>
+
+<MetaTags
+  title="IiAD 22/23"
+/>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+
+  <h1>Test</h1>
+</main>
+
