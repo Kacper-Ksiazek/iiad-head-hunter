8e2ebbd3973f0d61e15e5609b483485c1f8bc72a
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-03-02 15:44:22 +0100

    Button component added

---
+++ src/components/atoms/Button.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import {createEventDispatcher} from 'svelte'
+    import type {TailwindColor} from "@/types/tailwind";
+
+    const dispatch = createEventDispatcher();
+
+    export let disabled: boolean = false;
+    export let label: string = '';
+    export let variant: TailwindColor = 'gray';
+
+</script>
+
+<button
+        on:click={() => dispatch('click')}
+        disabled={disabled}
+        class="bg-{variant}-500 px-3 py-2 rounded text-white hover:bg-{variant}-600 transition duration-300
+        ease-in-out disabled:bg-stone-300 disabled:text-white">
+    {label}
+    <slot></slot>
+</button>
