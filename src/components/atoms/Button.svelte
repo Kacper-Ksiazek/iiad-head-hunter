28a97e884798fc0d684e42957ff071788cfb0397
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-03-04 08:20:16 +0100

    Button component problem with dynamic tailwind styles resolved

--- src/components/atoms/Button.svelte
+++ src/components/atoms/Button.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import {createEventDispatcher} from 'svelte'
+    import {TailwindPalette} from "@/types/tailwind";
+
     import type {TailwindColor} from "@/types/tailwind";
 
     const dispatch = createEventDispatcher();
@@ -8,14 +10,12 @@
     export let label: string;
     export let variant: TailwindColor = 'gray';
 
-
 </script>
 
 <button
         on:click={() => dispatch('click')}
         disabled={disabled}
-        class="bg-{variant}-500 px-3 py-2 rounded text-white hover:bg-{variant}-600 transition duration-300
-        ease-in-out disabled:bg-stone-300 disabled:text-white">
+        class="px-3 py-2 rounded transition duration-300 ease-in-out disabled:bg-stone-300 disabled:text-white {TailwindPalette.getColors(variant)}">
     {label}
     <slot></slot>
 </button>
